0b000c9c573d599fc1dcacef22cdf00f1796323a
Ryan Manalo <ryan.manalo@alum.berkeley.edu>
2013-06-10 13:46:38 -0700

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Club1000lb::Application.config.secret_token = '9852458638049fe4928e11457c84c62cf42054c068b8afa0b9c646086ab9de8453c1a8d0bc65d876dd723cd49555cac251d4276d2a2b41209de52f75ad2d17ff'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Club1000lb::Application.config.session_store :cookie_store, key: '_Club1000lb_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Club1000lb::Application.config.session_store :active_record_store
