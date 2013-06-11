8360958cd71a2cc84e9bf39bbbaa4fe1d81ef00e
Ryan Manalo <ryan.manalo@alum.berkeley.edu>
2013-06-11 15:14:20 -0700

    change variable names to be deadlift specific.

--- app/assets/javascripts/deadlift_data_points.js
+++ app/assets/javascripts/deadlift_data_points.js
@@ -1,5 +1,5 @@
-var data;
-var updatedData;
+var deadliftData;
+var deadliftUpdatedData;
 
 $.ajax({
   url: '/deadlift_data_points.json',
@@ -7,8 +7,8 @@ $.ajax({
   dataType: 'json',
   success: function(datapoints) {
     if(datapoints !== undefined){
-    data = datapoints.slice(0, (datapoints.length - 1));
-    updatedData = datapoints;
+    deadliftData = datapoints.slice(0, (datapoints.length - 1));
+    deadliftUpdatedData = datapoints;
     }
   },
   error: function () {
@@ -23,60 +23,58 @@ return Math.max.apply( Math, array );
 
 setTimeout(function() {
   $( document ).ready(function() {
-    var button = document.getElementById('button');
-    var container = document.getElementById('container');
-    var svg = d3.select('div#container svg');
+    var deadlift_button = document.getElementById('deadlift_button');
+    var deadlift_container = document.getElementById('deadlift_container');
+    var deadlift_svg = d3.select('div#deadlift_container svg');
 
-    var heightPoints = [];
-    var element = null;
+    var deadlift_heightPoints = [];
+    var deadlift_element;
 
-    for (var i = 0; i < updatedData.length; i++) {
-      element = updatedData[i];
-      heightPoints.push(element['data_point']);
+    for (var i = 0; i < deadliftUpdatedData.length; i++) {
+      deadlift_element = deadliftUpdatedData[i];
+      deadlift_heightPoints.push(deadlift_element['data_point']);
     }
 
-    $('div#container').width((updatedData.length * 50) + 50);
-    $('div#container').height(Array.max(heightPoints) + 50);
+    $('div#deadlift_container').width((deadliftUpdatedData.length * 50) + 50);
+    $('div#deadlift_container').height(Array.max(deadlift_heightPoints) + 50);
 
-    var lineFunction = d3.svg.line()
+    var deadlift_lineFunction = d3.svg.line()
       .x(function(d, i){ return (i * 50) + 50;})
-      .y(function(d){ return (Array.max(heightPoints) + 100) - d.data_point;})
+      .y(function(d){ return (Array.max(deadlift_heightPoints) + 100) - d.data_point;})
       .interpolate('linear');
 
-    var lastData = $(data).last();
-
-    svg.append("text")
+    deadlift_svg.append("text")
         .attr("class", "x label")
         .style("text-anchor", "middle")
-        .attr("x", ((updatedData.length * 50) + 50)/2)
-        .attr("y", (Array.max(heightPoints) + 50) - 5)
+        .attr("x", ((deadliftUpdatedData.length * 50) + 50)/2)
+        .attr("y", (Array.max(deadlift_heightPoints) + 50) - 5)
         .text("Date");
 
-    svg.append("text")
+    deadlift_svg.append("text")
         .attr("class", "y label")
         .style("text-anchor", "middle")
         .attr("y", 5)
-        .attr("x", -(Array.max(heightPoints) + 50)/2)
+        .attr("x", -(Array.max(deadlift_heightPoints) + 50)/2)
         .attr("dy", ".75em")
         .attr("transform", "rotate(-90)")
         .text("Weight (lb)");
 
-    svg.append('path')
+    deadlift_svg.append('path')
       .style("fill", "none")
       .style("stroke", "#799adc")
       .style("stroke-width", 5)
-      .attr("d", lineFunction(data));
+      .attr("d", deadlift_lineFunction(deadliftData));
 
 
-    button.onclick = function () {
-      svg.append('path')
+    deadlift_button.onclick = function () {
+      deadlift_svg.append('path')
         .transition()
         .duration(1000)
         .style("fill", "none")
         .style("stroke", "#4180f3")
         .style("stroke-width", 5)
-        .attr("d", lineFunction( updatedData ));
+        .attr("d", deadlift_lineFunction( deadliftUpdatedData ));
         this.onclick=null;
     };
   });
-},100);
+}, 100);
